c92d6e8df946bc25ac0ae571e41b9f8b2f456ea7
Rosyid <arrosyidbh@gmail.com>
2023-06-29 09:41:59 +0700

    Add table

--- src/pages/dashboard/scrapper/index.tsx
+++ src/pages/dashboard/scrapper/index.tsx
@@ -1,16 +1,53 @@
 import { type NextPage, type GetServerSideProps } from 'next'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { type ScrapeResult, type ScrapperPageProps } from 'types'
 import DashboardLayout from '~/layouts/Dashboard'
+import { MRT_ColumnDef, MaterialReactTable } from 'material-react-table';
 
-const Scrapper: NextPage<ScrapperPageProps> =  ({ host }) => {
-    const [scrapeData, setScrapeData] = useState<ScrapeResult[]>()
+interface LPSEItem {
+    no: number;
+    from: string;
+    type: string;
+    hps: string;
+    lasDate: string;
+    title: string;
+};
+
+const Scrapper: NextPage<ScrapperPageProps> = ({ host }) => {
+    const [scrapeData, setScrapeData] = useState<LPSEItem[]>([])
+
+    const columns = useMemo<MRT_ColumnDef<LPSEItem>[]>(
+        () => [
+            {
+                accessorKey: 'from',
+                header: 'Kementrian',
+            },
+            {
+                accessorKey: 'title',
+                header: 'Judul',
+            },
+            {
+                accessorKey: 'type',
+                header: 'Jenis',
+            },
+            {
+                accessorKey: 'hps',
+                header: 'HPS',
+            },
+            {
+                accessorKey: 'lasDate',
+                header: 'Akhir Pendaftaran',
+            },
+        ],
+        [],
+    );
 
     useEffect(() => {
         fetch(`${host}/api/data/`)
             .then((res) => res.json())
             .then((value) => {
                 const data: ScrapeResult[] = value?.result
+                data.map(scrape => scrape as LPSEItem)
                 setScrapeData(data)
                 console.log(data)
             })
@@ -19,54 +56,12 @@ const Scrapper: NextPage<ScrapperPageProps> =  ({ host }) => {
     return (
         <DashboardLayout>
             <div className="relative overflow-x-auto overflow-y-auto">
-                <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
-                    <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
-                        <tr>
-                            <th scope="col" className="px-6 py-3">
-                                Kementrian
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Judul
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Jenis
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                HPS
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Akhir Pendaftaran
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {scrapeData == undefined && (
-                            <tr>
-                                <td
-                                    colSpan={5}
-                                    className="text-center text-lg text-white"
-                                >
-                                    Loading ....
-                                </td>
-                            </tr>
-                        )}
-                        {scrapeData != undefined &&
-                            scrapeData.map((value) => (
-                                <tr
-                                    key={value.no}
-                                    className="border-b bg-white dark:border-gray-700 dark:bg-gray-800"
-                                >
-                                    <td className="px-6 py-4">{value.from}</td>
-                                    <td className="px-6 py-4">{value.title}</td>
-                                    <td className="px-6 py-4">{value.type}</td>
-                                    <td className="px-6 py-4">{value.hps}</td>
-                                    <td className="px-6 py-4">
-                                        {value.lasDate}
-                                    </td>
-                                </tr>
-                            ))}
-                    </tbody>
-                </table>
+                <MaterialReactTable
+                    columns={columns}
+                    data={scrapeData}
+                    enablePagination={false}
+                    enableRowVirtualization
+                />
             </div>
         </DashboardLayout>
     )
